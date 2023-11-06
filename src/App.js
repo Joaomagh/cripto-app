ca651449e9083ee0293e0cbf3d448ddb74e28527
João Pedro Rego Magalhães <joaomagh@gmail.com>
2023-11-06 02:51:47 -0300

    Resolva conflitos de merge

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
   return (
     <div className='coin-app'>
       <div className='coin-search'>
-        <h1 className='coin-text'>Busque Criptomoeda</h1>
+        <h1 className='coin-text'>Busque uma Criptomoeda</h1>
         <form>
           <input
             className='coin-input'
